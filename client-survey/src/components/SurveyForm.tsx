db4b9c744a24bc08b4bd10b77818a9c3d0a13464
Christian <prog.christian07@gmail.com>
2026-03-23 22:31:42 +0800

    Update useSurveyForm to include qrToken as a parameter

--- client-survey/src/components/SurveyForm.tsx
+++ client-survey/src/components/SurveyForm.tsx
@@ -33,7 +33,7 @@ const SurveyForm: React.FC<SurveyFormProps> = ({ office, qrToken }) => {
     errorMessage,
     isSuccessOpen,
     setIsSuccessOpen,
-  } = useSurveyForm(office);
+  } = useSurveyForm(office, qrToken);
 
   return (
     <div className="min-h-screen bg-gray-50 py-6 sm:py-8 px-4 sm:px-6 lg:px-8">
