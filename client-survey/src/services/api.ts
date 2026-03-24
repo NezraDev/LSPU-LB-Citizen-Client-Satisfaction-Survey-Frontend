bab7a3320f79410bd0db30229970354bd3e001fe
Christian <prog.christian07@gmail.com>
2026-03-24 10:42:30 +0800

    Remove environment configuration files and update API base URL handling in api service

--- client-survey/src/services/api.ts
+++ client-survey/src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { API_BASE_URL } from "../config/environment";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 const api = axios.create({
   baseURL: API_BASE_URL,
